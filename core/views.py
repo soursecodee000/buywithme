b42bc63ce28e0f008b0e90d499562d8ba75cb4ec
Muhammad Ahmad <ma7744746@gmail.com>
2024-09-10 18:45:16 +0500

    Update index.html and styles.css for product display

--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from store.models import Product
 
-# Create your views here.
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'core/index.html', {'products': products})
